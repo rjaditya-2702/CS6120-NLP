31094397cf8b386a923e1388687a356bdb6afd39
rjaditya-2702 <jannali.a@northeastern.edu>
2024-11-17 18:20:37 -0500

    Adding code to mask different heads. GPT2ForSequenceClassification has only 1 block and 12 heads. In this commit/ revision, we mask one attention head at a time and get the probability vectors for each of the model varaitions

--- code/main.py
+++ code/main.py
@@ -1,12 +1,14 @@
 import copy
 import torch
+import torch.nn as nn
 import numpy as np
 import pandas as pd
 
-from datasets import Dataset, DatasetDict
+from transformers import pipeline
 from transformers import GPT2Tokenizer
 from transformers import GPT2ForSequenceClassification, GPT2Config
 
+sm = nn.Softmax()
 
 def apply_mask(model, head_name):
     """
@@ -30,3 +32,53 @@ def apply_mask(model, head_name):
             param *= mask_bias
     
     return model_copy
+
+def main(text, tokenizer, main_model):
+
+    variation_list = []
+    n_heads = 12 # MHA
+
+    ## mask one attention head at a time:
+    for i in range(n_heads):
+        attention_head_name = f'transformer.h.{i}.attn.c_proj'
+        variation_list.append(apply_mask(main_model, attention_head_name))
+
+    ## collect prob vectors
+    output_dict = {}
+    for i, model in enumerate(variation_list):
+        inputs = tokenizer(text, return_tensors = 'pt')
+        with torch.no_grad():
+            logits = model(**inputs).logits
+        softmax_probs = sm(logits)
+        print(softmax_probs)
+        output_dict[i] = softmax_probs
+    return output_dict
+
+if __name__ == '__main__':
+
+    ## trained model:
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    model = GPT2ForSequenceClassification.from_pretrained("./model")
+    tokenizer.pad_token = tokenizer.eos_token
+
+    ## get examples per class:
+
+    # text = """
+    # I am happy becuase of this movie. The joy i experience is insurmountable.
+    # """
+    text = """Today is a good day. A lot of people minding their own business. None of them include checking on me though!"""
+    
+    print(len(text))
+    ## baseline result:
+    inputs = tokenizer(text, return_tensors = 'pt')
+    with torch.no_grad():
+        logits = model(**inputs).logits
+    probs = sm(logits).tolist()
+    print(probs)
+    print()
+
+    ## get the probabilities for each variation:
+    diff_probs = main(text, tokenizer, model)
+    print(diff_probs)
+
+    ## comparison:
