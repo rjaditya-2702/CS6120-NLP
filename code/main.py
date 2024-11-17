855d9f8e3740d7ed63fbd8466d67ac023b13643b
rjaditya-2702 <jannali.a@northeastern.edu>
2024-11-17 17:08:23 -0500

    Code to mask a given attention matrix. Restructured the codebase a bit. Next up: write code to pass the appropriate name for block i and head j to get a new copy of the model with the masked attention matrix of the specific block. head

--- code/main.py
+++ code/main.py
@@ -1,5 +1,32 @@
+import copy
 import torch
 import numpy as np
-import os
-import sys
+import pandas as pd
 
+from datasets import Dataset, DatasetDict
+from transformers import GPT2Tokenizer
+from transformers import GPT2ForSequenceClassification, GPT2Config
+
+
+def apply_mask(model, head_name):
+    """
+    :param: model - the trained model
+    :param: the attention matrix of the head to be masked. e.g. 'transformer.h.2.attn.c_proj'
+    """
+
+    weight_name = head_name + '.weights'
+    bias_name = head_name + '.bias'
+
+    mask_weight = torch.zeros((768, 768))
+    mask_bias = torch.zeros(768)
+
+    model_copy = copy.deepcopy(model)
+
+    for name, param in model_copy.named_parameters():
+        param.requires_grad = False
+        if name == weight_name:
+            param *= mask_weight
+        elif name == bias_name:
+            param *= mask_bias
+    
+    return model_copy
