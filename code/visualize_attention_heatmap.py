e8dda964ee2d83ddb96ead96758c1e53c81fe154
Yuchen-Zhang-Karen <karenzyc@gmail.com>
2024-12-04 15:26:33 -0500

    Plot difference heatmap

--- code/visualize_attention_heatmap.py
+++ code/visualize_attention_heatmap.py
@@ -1,4 +1,5 @@
 from nnsight import LanguageModel
+
 import matplotlib.pyplot as plt
 
 def plot_head0_heatmap(sent1, sent2, model_folder_path):
@@ -15,10 +16,11 @@ def plot_head0_heatmap(sent1, sent2, model_folder_path):
 
     h0 = h0[0]
     h1 = h1[0]
+    hDiff = h0 - h1
     vmin = min(h0.min(), h1.min())
     vmax = max(h0.max(), h1.max())
 
-    fig, axes = plt.subplots(2, 1, figsize=(20, 10), constrained_layout=True)
+    fig, axes = plt.subplots(3, 1, figsize=(20, 15), constrained_layout=True)
 
     im0 = axes[0].imshow(h0, aspect='auto', cmap='viridis', vmin=vmin, vmax=vmax)
     axes[0].set_title(sent1)
@@ -30,7 +32,13 @@ def plot_head0_heatmap(sent1, sent2, model_folder_path):
     axes[1].set_xlabel('Activation')
     axes[1].set_ylabel('Words')
 
+    im2 = axes[2].imshow(hDiff, aspect='auto', cmap='viridis')
+    axes[2].set_title(sent2)
+    axes[2].set_xlabel('Activation')
+    axes[2].set_ylabel('Words')
+
     # Add a shared colorbar
     cbar = fig.colorbar(im0, ax=axes[0], orientation='vertical', fraction=0.02, pad=0.02)
     cbar = fig.colorbar(im1, ax=axes[1], orientation='vertical', fraction=0.02, pad=0.02)
+    cbar = fig.colorbar(im2, ax=axes[2], orientation='vertical', fraction=0.02, pad=0.02)
     cbar.set_label('Value')
